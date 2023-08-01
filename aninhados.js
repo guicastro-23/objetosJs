a76e36e6027148da6c5f8874d77c930efae1f240
guicastro-23 <guilherme.gcastro20@gmail.com>
2023-07-31 23:32:08 -0300

    tipos aninhados

---
+++ aninhados.js
@@ -0,0 +1,15 @@
+const cliente ={
+    nome: "Joao",
+    idade: 24, 
+    email: "joaomentira@leite.com",
+    telefone: ["42 988765435", "11890074232"]
+};
+
+cliente.endereco = {
+    rua: "R. Joseph Climber",
+    numero: 160,
+    apartamento: true,
+    complemento: "ap 202",
+}
+
+console.log(cliente.endereco)
---
+++ tipos.js
@@ -0,0 +1,8 @@
+const cliente ={
+    nome: "Joao",
+    idade: 24, 
+    email: "joaomentira@leite.com",
+    telefone: ["42 988765435", "11890074232"]
+};
+
+console.log(cliente)
